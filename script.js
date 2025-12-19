68011b14bd07d3074dc2cfafed94c468ce7db2a3
Arul Rajan <arulrajan22@gmail.com>
2025-12-19 22:32:13 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,27 +1,32 @@
 const textEl = document.getElementById("typed-text");
 
-const words = ["I am Ironman", "I am Batman", "I am Arul..."];
+const prefix = "I am ";
+const words = ["Ironman", "Batman", "Arul..."];
+
 let wordIndex = 0;
 let charIndex = 0;
 let deleting = false;
+
+textEl.textContent = prefix;
 
 function typeLoop() {
   const currentWord = words[wordIndex];
 
   if (!deleting) {
-    textEl.textContent = currentWord.slice(0, charIndex++);
+    textEl.textContent = prefix + currentWord.slice(0, charIndex++);
     if (charIndex > currentWord.length) {
       setTimeout(() => deleting = true, 1000);
     }
   } else {
-    textEl.textContent = currentWord.slice(0, charIndex--);
+    textEl.textContent = prefix + currentWord.slice(0, charIndex--);
     if (charIndex < 0) {
       deleting = false;
+      charIndex = 0;
       wordIndex = (wordIndex + 1) % words.length;
     }
   }
 
-  setTimeout(typeLoop, deleting ? 60 : 120);
+  setTimeout(typeLoop, deleting ? 70 : 120);
 }
 
 typeLoop();
